3fbb29e95de5cba8732a8b28ed19a08aa83ff9a7
ks200459 <35667732+ks200459@users.noreply.github.com>
2018-01-24 11:37:40 -0800

    Playing with mnist_softmax.py
    Editing tensorflow/examples/tutorials/mnist/mnist_softmax.py

--- tensorflow/examples/tutorials/mnist/mnist_softmax.py
+++ tensorflow/examples/tutorials/mnist/mnist_softmax.py
@@ -45,6 +45,7 @@ def main(_):
   # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
 
+  # Understand raw formulation and the built-in function
   # The raw formulation of cross-entropy,
   #
   #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
